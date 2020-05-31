b4b62bde5c5b3c8608b424f2b64d7465c924284b
Carlos Castro <cpac20.4@gmail.com>
2020-05-31 20:10:02 +0100

    wip

--- trabalho/graphs.py
+++ trabalho/graphs.py
@@ -6,12 +6,11 @@ import matplotlib.pyplot as plt
 
 
 class Interval:
-    def __init__(self, med, min, max, med_label, int_label):
+    def __init__(self, med, min, max, med_label):
         self.med = med
         self.min = min
         self.max = max
         self.med_label = med_label
-        self.int_label = int_label
     
 
 def graph_multi(x, title, x_label, y_label, *args):
@@ -20,7 +19,7 @@ def graph_multi(x, title, x_label, y_label, *args):
 
     for arg in args:  
         ax.plot(x, arg.med, '-', label=arg.med_label)
-        ax.fill_between(x, arg.min, arg.max, alpha=0.2, label=arg.int_label)
+        ax.fill_between(x, arg.min, arg.max, alpha=0.2)
 
     ax.set_title(title)
     ax.set_xlabel(x_label)
@@ -30,15 +29,15 @@ def graph_multi(x, title, x_label, y_label, *args):
     plt.show()
 
 
-def graph_vs(x, med1, min1, max1, med_label1, int_label1, med2, min2, max2, med_label2, int_label2, title, x_label, y_label):
+def graph_vs(x, med1, min1, max1, med_label1, med2, min2, max2, med_label2, title, x_label, y_label):
 
     fig, ax = plt.subplots()
 
     ax.plot(x, med1, label=med_label1)
-    ax.fill_between(x, min1, max1, alpha=0.2, label=int_label1)
+    ax.fill_between(x, min1, max1, alpha=0.2)
 
     ax.plot(x, med2, label=med_label2)
-    ax.fill_between(x, min2, max2, alpha=0.2, label=int_label2)
+    ax.fill_between(x, min2, max2, alpha=0.2)
 
     ax.set_title(title)
     ax.set_xlabel(x_label)
@@ -58,15 +57,15 @@ def average_vs_count(r):
 
     graph_vs(
         nodes,
-        one["med_messages"], one["min_messages"], one["max_messages"], 'Average (AVERAGE)', 'Interval (AVERAGE)', 
-        two["med_messages"], two["min_messages"], two["max_messages"], 'Average (COUNT)', 'Interval (COUNT)', 
+        one["med_messages"], one["min_messages"], one["max_messages"], 'AVERAGE', 
+        two["med_messages"], two["min_messages"], two["max_messages"], 'COUNT', 
         "Messages comparison AVERAGE vs COUNT", 
         "Nodes", "Messages")
 
     graph_vs(
         nodes, 
-        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'Average (AVERAGE)', 'Interval (AVERAGE)', 
-        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'Average (COUNT)', 'Interval (COUNT)', 
+        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'AVERAGE', 
+        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'COUNT', 
         "Rounds comparison AVERAGE vs COUNT", 
         "Nodes", "Rounds")
 
@@ -81,15 +80,15 @@ def rmse_vs_flowsum(r):
 
     graph_vs(
         nodes,
-        one["med_messages"], one["min_messages"], one["max_messages"], 'Average (RMSE)', 'Interval (RMSE)', 
-        two["med_messages"], two["min_messages"], two["max_messages"], 'Average (FLOWSUM)', 'Interval (FLOWSUM)', 
+        one["med_messages"], one["min_messages"], one["max_messages"], 'RMSE',
+        two["med_messages"], two["min_messages"], two["max_messages"], 'FLOWSUM',
         "Messages comparison RMSE vs FLOWSUM termination", 
         "Nodes", "Messages")
 
     graph_vs(
         nodes, 
-        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'Average (RMSE)', 'Interval (RMSE)', 
-        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'Average (FLOWSUM)', 'Interval (FLOWSUM)', 
+        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'RMSE',
+        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'FLOWSUM',
         "Rounds comparison RMSE vs FLOWSUM termination", 
         "Nodes", "Rounds")
 
@@ -98,10 +97,9 @@ def rmse_vs_flowsum(r):
 def multicasts(r):
     graph_multi(
         r["normal"]["step_axis"], "Comparison", "Nodes", "Rounds",
-        Interval(r["normal"]["med_rounds"],r["normal"]["min_rounds"],r["normal"]["max_rounds"],"No loss","No loss"),
-        Interval(r["02"]["med_rounds"],r["02"]["min_rounds"],r["02"]["max_rounds"],"20% message loss","20% message loss"),
-        Interval(r["04"]["med_rounds"],r["04"]["min_rounds"],r["04"]["max_rounds"],"40% message loss","40% message loss"),
-        Interval(r["06"]["med_rounds"],r["06"]["min_rounds"],r["06"]["max_rounds"],"60% message loss","60% message loss")
+        Interval(r["normal"]["med_rounds"],r["normal"]["min_rounds"],r["normal"]["max_rounds"],"Broadcast"),
+        Interval(r["uni"]["med_rounds"],r["uni"]["min_rounds"],r["uni"]["max_rounds"],"Unicast"),
+        Interval(r["euni"]["med_rounds"],r["euni"]["min_rounds"],r["euni"]["max_rounds"],"Evaluated Unicast")
         )
 
 
@@ -110,11 +108,11 @@ def rounds_rmse_loss(r, rmse):
 
     # normal/broadcast
     graph_multi(
-        rmse, "Broadcast", "RMSE", "Rounds",
-        Interval(r["0"]["med_rounds"],r["0"]["min_rounds"],r["0"]["max_rounds"],"No loss","No loss"),
-        Interval(r["02"]["med_rounds"],r["02"]["min_rounds"],r["02"]["max_rounds"],"20% message loss","20% message loss"),
-        Interval(r["04"]["med_rounds"],r["04"]["min_rounds"],r["04"]["max_rounds"],"40% message loss","40% message loss"),
-        Interval(r["06"]["med_rounds"],r["06"]["min_rounds"],r["06"]["max_rounds"],"60% message loss","60% message loss")
+        rmse, "Broadcast with different loss rates", "RMSE", "Rounds",
+        Interval(r["0"]["med_rounds"],r["0"]["min_rounds"],r["0"]["max_rounds"],"No loss"),
+        Interval(r["02"]["med_rounds"],r["02"]["min_rounds"],r["02"]["max_rounds"],"20% message loss"),
+        Interval(r["04"]["med_rounds"],r["04"]["min_rounds"],r["04"]["max_rounds"],"40% message loss"),
+        Interval(r["06"]["med_rounds"],r["06"]["min_rounds"],r["06"]["max_rounds"],"60% message loss")
         )
 
 
@@ -178,15 +176,15 @@ def sync_vs_async(r, r2):
 
     graph_vs(
         nodes,
-        one["med_messages"], one["min_messages"], one["max_messages"], 'Average (sync)', 'Interval (sync)', 
-        two["med_messages"], two["min_messages"], two["max_messages"], 'Average (async)', 'Interval (async)', 
+        one["med_messages"], one["min_messages"], one["max_messages"], 'Average (sync)',
+        two["med_messages"], two["min_messages"], two["max_messages"], 'Average (async)',
         "Messages comparison sync vs async", 
         "Nodes", "Messages")
 
     graph_vs(
         nodes, 
-        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'Average (sync)', 'Interval (sync)', 
-        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'Average (async)', 'Interval (async)', 
+        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'Average (sync)',
+        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'Average (async)',
         "Rounds comparison sync vs async", 
         "Nodes", "Rounds")
 
@@ -200,15 +198,15 @@ def async_vs_asynct(r, r2):
 
     graph_vs(
         nodes,
-        one["med_messages"], one["min_messages"], one["max_messages"], 'Average', 'Interval', 
-        two["med_messages"], two["min_messages"], two["max_messages"], 'Average (with timeout)', 'Interval (with timeout)', 
+        one["med_messages"], one["min_messages"], one["max_messages"], 'Average',
+        two["med_messages"], two["min_messages"], two["max_messages"], 'Average (with timeout)',
         "Messages comparison async vs async with timepout", 
         "Nodes", "Messages")
 
     graph_vs(
         nodes, 
-        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'Average', 'Interval', 
-        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'Average (with timeout)', 'Interval (with timeout)', 
+        one["med_rounds"], one["min_rounds"], one["max_rounds"], 'Average',
+        two["med_rounds"], two["min_rounds"], two["max_rounds"], 'Average (with timeout)',
         "Rounds comparison async vs async with timeout", 
         "Nodes", "Rounds")
 
@@ -256,24 +254,49 @@ def rmse_vs_flowsum_exec():
 def rounds_rmse_loss_exec():
 
 
-    b1 = builders.SimulatorBuilder().with_agregation_type('count').with_loss_rate(0)
-    b2 = builders.SimulatorBuilder().with_agregation_type('count').with_loss_rate(0.2)
-    b3 = builders.SimulatorBuilder().with_agregation_type('count').with_loss_rate(0.4)
-    b4 = builders.SimulatorBuilder().with_agregation_type('count').with_loss_rate(0.6)
+    b1 = builders.SimulatorBuilder().with_agregation_type('average').with_loss_rate(0)
+    b2 = builders.SimulatorBuilder().with_agregation_type('average').with_loss_rate(0.2)
+    b3 = builders.SimulatorBuilder().with_agregation_type('average').with_loss_rate(0.4)
+    b4 = builders.SimulatorBuilder().with_agregation_type('average').with_loss_rate(0.6)
     
     bs = {'0' : b1, '02' : b2, '04' : b3, '06' : b4}
     
-    G = graphGen.randomG(9,3,30)
-    rmses = [10, 1, 0.1, 0.01]
-    thread_args = ([G], 1, bs)
+    Graphs = []
+    for i in range(10):
+        Graphs.append(graphGen.randomG(100,5,10))
+
+    rmses = [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.20, 0.25, 0.5]
+    thread_args = (Graphs, bs)
     print("start execution")
     final_results = rmse_step_execution(rmses, 2, bs, thread_args)
-    print(final_results)
+    #print(final_results)
     rounds_rmse_loss(final_results, rmses)
 
-
+# TODO
 def casts_comparison_exec():
+
+    iters = 3
+    max_degree = 3
+
+    b1 = builders.SimulatorBuilder().with_agregation_type('average')
+    b2 = builders.SimulatorBuilder().with_agregation_type('average').with_multicast_protocol(1)
+    b3 = builders.SimulatorBuilder().with_agregation_type('average').with_evaluated_multicast_protocol(1)
+
+    bs = {'normal' : b1, 'uni' : b2, 'euni' : b3}
+
+    thread_args = (max_degree, iters, bs, 10)
+
+    print("start execution")
+    final_results = node_step_execution(5, 25, 5, 2, bs, thread_args)
+    all_estimates = final_results['uni']['nodes_estimates'][0]
+    final_e = all_estimates[len(all_estimates) -1]
+    print("isto -> ", final_e)
+
     multicasts(final_results)
+
+
+
+
 
 def rounds_rmse_node_entry_exec():
 
@@ -412,8 +435,8 @@ def async_vs_async_no_timeout_exec():
 if __name__ == '__main__': 
     #average_vs_count_exec()
     #rounds_rmse_dynamic_exec()
-    #rounds_rmse_loss_exec()
-    casts_comparison_exec()
+    rounds_rmse_loss_exec()
+    #casts_comparison_exec()
     #min_dif_average_exec()
     #sync_vs_async_exec()
     #async_vs_async_no_timeout_exec()
